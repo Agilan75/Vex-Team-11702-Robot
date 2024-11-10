676603b0083041a77b5eeb64c9594aee6f439a7f
Agilan Hariharan <agilanh100@gmail.com>
2024-11-10 10:28:42 -0500

    drivetrain testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       agilanhariharan                                           */
+/*    Author:       Team 11702                                                */
 /*    Created:      11/5/2024, 6:11:26 PM                                     */
 /*    Description:  V5 project                                                */
 /*                                                                            */
@@ -10,19 +10,47 @@
 
 using namespace vex;
 
-// A global instance of vex::brain used for printing to the V5 brain screen
-vex::brain       Brain;
+// initalize things
+brain Brain;
+controller Controller;
+motor leftMotorA = motor(PORT1, false);
+motor leftMotorB = motor(PORT2, false);
+motor rightMotorA = motor(PORT3, true);
+motor rightMotorB = motor(PORT4, true);
 
-// define your global instances of motors and other devices here
+// group motors
+motor_group leftMotors(leftMotorA, leftMotorB);
+motor_group rightMotors(rightMotorA, rightMotorB);
 
+// 4 motor object
+drivetrain Drivetrain = drivetrain(leftMotors, rightMotors, 259.34, 320, 40, mm, 1);
+
+void userControlLoop() {
+    // inital velocity
+    Drivetrain.setDriveVelocity(100, percent);
+    Drivetrain.setStopping(brake);  // stopping for brakw
+
+    while (true) {
+        // get valyes for joystick positon
+        int forwardSpeed = Controller.Axis3.position();
+        int turnSpeed = Controller.Axis4.position();   
+
+        // get speeds
+        int leftSpeed = forwardSpeed + turnSpeed;
+        int rightSpeed = forwardSpeed - turnSpeed;
+
+        // controls to move
+        leftMotors.spin(forward, leftSpeed, percent);
+        rightMotors.spin(forward, rightSpeed, percent);
+
+        if (forwardSpeed == 0 && turnSpeed == 0) {
+            Drivetrain.stop();
+        }
+
+    }
+}
 
 int main() {
-
-    Brain.Screen.printAt( 10, 50, "Hello V5" );
-   
-    while(1) {
-        
-        // Allow other tasks to run
-        this_thread::sleep_for(10);
-    }
+    vexcodeInit();
+    userControlLoop();
 }
