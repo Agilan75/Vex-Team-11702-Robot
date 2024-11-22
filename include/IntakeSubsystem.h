e5204c88b6a9732c889298c7f890f62d76987f1f
Ray Tan <tanjiav@gmail.com>
2024-11-21 23:30:09 -0500

    reformat = structrue

---
+++ include/IntakeSubsystem.h
@@ -0,0 +1,17 @@
+#ifndef INTAKESUBSYSTEM_H
+#define INTAKESUBSYSTEM_H
+
+#include "vex.h"
+using namespace vex;
+
+class IntakeSubsystem {
+private:
+    motor intake;
+
+public:
+    IntakeSubsystem();
+    void setIntake(double speed); 
+    void stop(); 
+};
+
+#endif
