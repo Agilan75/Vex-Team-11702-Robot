32ad50604bdc8a87c0fda8fd4d5792c0f310da1c
Ray Tan <tanjiav@gmail.com>
2024-11-22 15:28:04 -0500

    New features to commands

--- include/Commands/DriveCommand.h
+++ include/Commands/DriveCommand.h
@@ -2,19 +2,20 @@
 #define DRIVECOMMAND_H
 
 #include "Subsystems/DriveSubsystem.h"
+#include <functional> 
 
 class DriveCommand{
 private:
     DriveSubsystem* driveSubsystem;
-    int leftSpeed;
-    int rightSpeed;
-    bool isFinished;
+    std::function<double()> leftInputProvider; 
+    std::function<double()> rightInputProvider;
+    bool isFinishedCheck;
 
 public:
-    DriveCommand(DriveSubsystem *dt, int leftSpeed, int rightSpeed);
+    DriveCommand(DriveSubsystem *dt, std::function<double()> leftSpeed, std::function<double()> rightSpeed);
     void initialize();
     void execute();
-    bool isComplete();
+    bool isFinished();
     void end();
 };
 
--- src/Commands/DriveCommand.cpp
+++ src/Commands/DriveCommand.cpp
@@ -1,14 +1,31 @@
 #include "DriveCommand.h"
+#include "DriveSubsystem.h"
 #include "vex.h"
+#include <functional>
+#include <iostream>
+#include "Log.h"
 
-DriveCommand ::DriveCommand(DriveSubsystem *dt, int leftSpeed, int rightSpeed) :
+DriveCommand ::DriveCommand(DriveSubsystem *dt, std::function<double()> leftSpeed, std::function<double()> rightSpeed) :
     driveSubsystem(dt),
-    leftSpeed(leftSpeed),
-    rightSpeed(rightSpeed),
-    isFinished(false)
+    leftInputProvider(leftSpeed),
+    rightInputProvider(rightSpeed),
+    isFinishedCheck(false)
 {}
 
 void DriveCommand :: initialize() {
-    driveSubsystem->drive(leftSpeed, rightSpeed);
-     
+}
+
+void DriveCommand ::execute(){
+    driveSubsystem->drive(leftInputProvider(), rightInputProvider());
+    Log::log("rightSpeed", leftInputProvider());
+    Log::log("rightSpeed", rightInputProvider());
+}
+
+void DriveCommand ::end()
+{
+    driveSubsystem->stop();
+}
+
+bool DriveCommand ::isFinished() {
+    return isFinishedCheck;
 }
