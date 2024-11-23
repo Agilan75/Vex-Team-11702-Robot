2f3f232fb4f84823e0410bb67ba72ceef5f957cf
Eric Chen <zhenxuanchen2@gmail.com>
2024-11-22 19:46:33 -0500

    added drivetrain into main logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,36 @@
 #include "vex.h"
+#include "DriveSubsystem.h"
+#include "MogoSubsystem.h"
+#include "IntakeSubsystem.h"
+#include "DriveCommand.h"
 
 using namespace vex;
 
+// create objects
 brain Brain;
 controller Controller;
+competition Competition;
+DriveSubsystem driveSubsystem;
+MogoSubsystem mogoSubsystem;
+IntakeSubsystem intakeSubsystem;
 
-motor leftMotorA = motor(PORT1, false);
-motor leftMotorB = motor(PORT2, false);
-motor leftMotorC = motor(PORT3, false);
-motor rightMotorA = motor(PORT4, true);
-motor rightMotorB = motor(PORT5, true);
-motor rightMotorC = motor(PORT6, true);
+void auton()
+{
+    // auton in here eventually
+}
 
-motor_group leftMotors(leftMotorA, leftMotorB, leftMotorC);
-motor_group rightMotors(rightMotorA, rightMotorB, rightMotorC);
-
-drivetrain Drivetrain = drivetrain(leftMotors, rightMotors, 259.34, 320, 40, mm, 1);
-
-motor intakeMotor = motor(PORT7, false); 
-
-double Kp = 0.3;
-double Ki = 0.0;
-double Kd = 0.05;
-
-double leftError, rightError;
-double leftPrevError = 0, rightPrevError = 0;
-double leftIntegral = 0, rightIntegral = 0;
-double leftDerivative, rightDerivative;
-double leftTargetSpeed, rightTargetSpeed;
-
-// capping integral so dont go too big to the point it dont work
-double maxIntegral = 50;
-
-// for joystick sensitivity when touching and inputs
-int deadband = 5;
-int scaleFactor = 3;
-
-void userControlLoop() {
-    Drivetrain.setStopping(brake);
-
-    while (true) {
-        // for joystcik movement
-        int forwardSpeed = Controller.Axis2.position() * scaleFactor;
-        int turnSpeed = Controller.Axis1.position() * scaleFactor;
-
-        if (abs(forwardSpeed) < deadband && abs(turnSpeed) < deadband) {
-            forwardSpeed = 0;
-            turnSpeed = 0;
-        }
-
-        // speeds
-        leftTargetSpeed = forwardSpeed + turnSpeed;
-        rightTargetSpeed = forwardSpeed - turnSpeed;
-        double leftCurrentSpeed = leftMotors.velocity(percent);
-        double rightCurrentSpeed = rightMotors.velocity(percent);
-
-        // pid for left motor
-        leftError = leftTargetSpeed - leftCurrentSpeed;
-        leftIntegral += leftError;
-        leftDerivative = leftError - leftPrevError;
-        double leftOutput = (Kp * leftError) + (Ki * leftIntegral) + (Kd * leftDerivative);
-        leftMotors.spin(forward, leftOutput, percent);
-        leftPrevError = leftError;
-
-        // pid for right motor
-        rightError = rightTargetSpeed - rightCurrentSpeed;
-        rightIntegral += rightError;
-        rightDerivative = rightError - rightPrevError;
-        double rightOutput = (Kp * rightError) + (Ki * rightIntegral) + (Kd * rightDerivative);
-        rightMotors.spin(forward, rightOutput, percent);
-        rightPrevError = rightError;
-
-        if (forwardSpeed == 0 && turnSpeed == 0) {
-            leftMotors.stop();
-            rightMotors.stop();
-        }
-
-        vex::task::sleep(20);
-
-        // for intake
-        if (Controller.ButtonR1.pressing()) {
-            intakeMotor.spin(forward, 100, percent); 
-        } else if (Controller.ButtonR2.pressing()) {
-            intakeMotor.spin(reverse, 100, percent);
-        } else {
-            intakeMotor.stop(); 
-        }
-
-        // pneumatic clamp
-        // if (Controller.ButtonL1.pressing()) {
-        //     clampPneumatics.extend(); 
-        // } else if (Controller.ButtonL2.pressing()) {
-        //     clampPneumatics.retract(); 
-        // }
+void userControlLoop()
+{
+    while (true)
+    {
+        DriveCommand driveCommand(&driveSubsystem, 1000, 1000);
+        wait(20, sec);
     }
 }
 
-int main() {
+int main()
+{
+    Competition.autonomous(auton);
+    Competition.drivercontrol(userControlLoop);
     userControlLoop();
 }
